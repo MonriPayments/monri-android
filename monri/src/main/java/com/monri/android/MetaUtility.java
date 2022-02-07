3e94cacdf7b8a3d78b88478af8ee267b503fb702
adnanomerovic <1812992171632>
2022-02-07 15:52:18 +0100

    Worked on meta data
    worked on meta data.
    
    WP-925

--- monri/src/main/java/com/monri/android/MetaUtility.java
+++ monri/src/main/java/com/monri/android/MetaUtility.java
@@ -6,9 +6,22 @@ import android.preference.PreferenceManager;
 
 import androidx.annotation.NonNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 class MetaUtility {
     private static final String PREFERENCE_META_LIBRARY_KEY = "com.monri.meta.library";
     private static final String LIBRARY = "Android-SDK";
+
+    public static final String INTEGRATION_TYPE_KEY = "integration_type";
+    public static final String LIBRARY_KEY = "library";
+    public static final String LIBRARY_VERSION_KEY = "library_version";
+
+    public static final List<String> META_KEYS = Arrays.asList(
+            INTEGRATION_TYPE_KEY,
+            LIBRARY_KEY,
+            LIBRARY_VERSION_KEY
+    );
 
     private MetaUtility() {
     }
