a407fe9a47f97f0823f53b1ea8812972bfc415c7
adnanomerovic <1812992171632>
2023-01-13 15:17:14 +0100

    MNRI-309 Customers API minor changes, bumped SDK version to 1.4.0

--- monri/src/main/java/com/monri/android/MonriApi.java
+++ monri/src/main/java/com/monri/android/MonriApi.java
@@ -2,12 +2,6 @@ package com.monri.android;
 
 import com.monri.android.model.ConfirmPaymentParams;
 import com.monri.android.model.ConfirmPaymentResponse;
-import com.monri.android.model.MerchantCustomers;
-import com.monri.android.model.CustomerPaymentMethodResponse;
-import com.monri.android.model.Customer;
-import com.monri.android.model.RetrieveCustomerViaMerchantCustomerUuidParams;
-import com.monri.android.model.GetCustomerParams;
-import com.monri.android.model.CustomerPaymentMethodParams;
 import com.monri.android.model.PaymentStatusParams;
 import com.monri.android.model.PaymentStatusResponse;
 
